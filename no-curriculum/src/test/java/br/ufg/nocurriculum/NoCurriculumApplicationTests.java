809eb1fe65f654d9d940aa0bd0b0015f38cfdbee
mateusvoltolim <mateus.voltolim@gmail.com>
2023-05-30 21:26:40 -0300

    feature: init project

---
+++ no-curriculum/src/test/java/br/ufg/nocurriculum/NoCurriculumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufg.nocurriculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoCurriculumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
