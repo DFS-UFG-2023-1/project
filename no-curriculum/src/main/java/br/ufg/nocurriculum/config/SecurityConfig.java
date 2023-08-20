af2daff752532f3475274dda54294cf47266bf37
mateusvoltolim <mateus.voltolim@gmail.com>
2023-08-20 13:51:38 -0300

    chore: remove comments

--- no-curriculum/src/main/java/br/ufg/nocurriculum/config/SecurityConfig.java
+++ no-curriculum/src/main/java/br/ufg/nocurriculum/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -22,36 +21,18 @@ public class SecurityConfig extends VaadinWebSecurity {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // Delegating the responsibility of general configurations
-        // of http security to the super class. It's configuring
-        // the followings: Vaadin's CSRF protection by ignoring
-        // framework's internal requests, default request cache,
-        // ignoring public views annotated with @AnonymousAllowed,
-        // restricting access to other views/endpoints, and enabling
-        // ViewAccessChecker authorization.
-        // You can add any possible extra configurations of your own
-        // here (the following is just an example):
-
-        // http.rememberMe().alwaysRemember(false);
-
-        // Configure your static resources with public access before calling
-        // super.configure(HttpSecurity) as it adds final anyRequest matcher
         http.authorizeHttpRequests(
             it -> it.requestMatchers(
                 new AntPathRequestMatcher("/public/**")
             ).permitAll()
-        );
+        ).rememberMe(it -> it.alwaysRemember(true));
 
         super.configure(http);
-
-        // This is important to register your login view to the
-        // view access checker mechanism:
         setLoginView(http, LoginView.class);
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        // Customize your WebSecurity configuration.
         super.configure(web);
     }
 
